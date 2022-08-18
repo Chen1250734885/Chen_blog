ff9ce84b038f463dcbb70bdf30b32f16029fa6d7
Chenwei <1250734885@qq.com>
2022-08-18 09:35:15 +0800

    1.修复首页上面Summary of issues at work跳转404的问题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -61,10 +61,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 text: 'JS设计模式总结',
                 link: '/pages/4643cd/',
               },
-              {
-                text: 'Summary of issues at work',
-                link: '/pages/chenwork/',
-              },
             ],
           },
         ],
@@ -85,6 +81,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: 'Summary of issues at work', link: '/pages/6b2a17/' },
         ],
       },
       {
